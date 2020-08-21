a1e27810fa225d40ef3a5677161e3b6af465eaf2
tesso <ta4780@icloud.com>
2020-08-21 17:18:38 +0900

    簡単なコンポーネント

---
+++ src/components/MyButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button>{{ buttonName }}</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Mybutton",
+  props: {
+    buttonName: String
+  }
+});
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <MyButton buttonName = "Hello World"></MyButton>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import MyButton from "@/components/MyButton.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    MyButton
   }
 });
 </script>
